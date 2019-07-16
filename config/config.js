9ff3bb23606f3c9f776136d7b818416c88833d70
Gerardo Cetzal <gerardoce23@gmail.com>
2019-07-16 00:10:44 -0500

    issue

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//Por favor ingresa tus credenciales de GOOGLE_CLIENT_ID
+module.exports.GOOGLE_CLIENT_ID = '';
+module.exports.GOOGLE_SECRET = '';
